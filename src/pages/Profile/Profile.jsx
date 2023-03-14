c9cff48614bfb7e1b9c5c5bae48112f8cd3007a3
Pablo Villedae <pablov.e12345@gmail.com>
2023-03-14 14:21:46 -0700

    profile page styling:

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,11 @@
+import FriendCard from "../../components/FriendCard/FriendCard";
 const Profile = () => {
   return ( 
     <>
     <h1>this is Profile page</h1>
+    <FriendCard/>
     </>
   );
 }
- 
+
 export default Profile;
