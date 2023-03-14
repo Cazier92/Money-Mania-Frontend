0bc3f864caa0a94523c4f04dbdfe987883d1870d
Pablo Villedae <pablov.e12345@gmail.com>
2023-03-14 16:05:00 -0700

    add stats and friend cinditional rendering

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,17 @@
 import FriendCard from "../../components/FriendCard/FriendCard";
 import styles from './Profile.module.css'
+import { useState } from "react";
 const Profile = () => {
+
+  const [view, setView] = useState("stats")
+
+  function statView(){
+    setView("stats")
+  }
+  function friendView(){
+    setView("friend")
+  }
+
   return ( 
     <>
       <div className={styles.profileHeader}>
@@ -10,23 +21,28 @@ const Profile = () => {
       </div>
       <div className={styles.linkSection}>
         <p>
-          <button className={styles.linkHeader}>
+          <button className={styles.linkHeader} onClick={friendView}>
             Friends
           </button> 
         </p>
         <p > 
-          <button className={styles.profilePic}>
+          <button className={styles.profilePic} >
             <i id="bt-icon" class="fa-solid fa-user "></i>
           </button> 
         </p>
         <p>
-          <button className={styles.linkHeader}>
+          <button className={styles.linkHeader} onClick={statView}>
             Stats
           </button> 
         </p>
       </div>
       <hr />
+      {view === "friend" && 
       <FriendCard/>
+      }
+      {view === "stats" && 
+        <p>stats lol</p>
+      }
     </>
   );
 }
