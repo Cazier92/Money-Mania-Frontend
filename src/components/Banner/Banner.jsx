c6a5b5b837c398a77e951c6319287de19a238422
Cazier92 <bryce.cazier@gmail.com>
2023-03-15 16:04:49 -0700

    remove home circle, fix navBar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,8 @@ import styles from './Banner.module.css'
 const Banner = ({user, userProfile}) => {
     return (
         <main>
-            <div className={styles.circle}>
-            </div>
+            {/* <div className={styles.circle}>
+            </div> */}
                 <h2>Money Mania</h2>
                 <p className={styles.firstp}>Welcome back, </p>
                 {userProfile.name ? (<>
