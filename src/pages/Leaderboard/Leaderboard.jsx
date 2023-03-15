97f543614310b3737db533f23080d75faac1bf02
cbowman422 <chrisb0wman@hotmail.com>
2023-03-15 17:12:17 -0400

    leaderboard divs and classnames css

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,8 +1,30 @@
+import './Leaderboard.css'
+
 const Leaderboard = () => {
   return ( 
-    <>
-    <h1>This is leaderboard component</h1>
-    </>
+    <div className="leaderboardContainer">
+    <h3>Leaderboard</h3>
+    <div className="leaderboardStage">
+        <div className="third">
+          <image className="stageImage">
+          </image>
+          <p>name</p>
+          <div className="thirdStage"></div>
+        </div>
+        <div className="first">
+          <image  className="stageImage">
+          </image>
+          <p>name</p>
+          <div className="firstStage"></div>
+        </div>
+        <div className="second">
+          <image className="stageImage">
+          </image>
+          <p>name</p>
+          <div className="secondStage"></div>
+        </div>
+    </div>
+    </div>
   );
 }
 
