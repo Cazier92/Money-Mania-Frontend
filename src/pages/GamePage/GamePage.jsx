08a522c75f2e23dc7eaa4087ce786c71bd6ba45d
Cazier92 <bryce.cazier@gmail.com>
2023-03-15 14:09:13 -0700

    introduce conditional rendering when a user answers a tenth question/tenth multiple question

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -5,12 +5,13 @@ import AnswerCard from "../../components/AnswerCard/AnswerCard";
 
 import './GamePage.css'
 
-const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpdated}) => {
+const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpdated, userProfile, qNum, rewardClaimed, setRewardClaimed}) => {
   const [displayWin, setDisplayWin] = useState(false)
   const [correct, setCorrect] = useState(false)
   const [id, setId] = useState('')
   const fillerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   let answer
+  let q = qNum % 10
 
   if (currentTrivia.ans === 'a') {
     answer = currentTrivia.a
@@ -37,45 +38,60 @@ const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpda
   }
 
   
-
   return ( 
-    <div className="game-page-main">
-      {displayWin ? 
-      (<>
-        {correct ? 
-        (<div className="correct-ans-div">
-          <h1>Good Job!</h1>
-          <div>
-            <QuestionCard currentTrivia={currentTrivia}/>
-            <h5>Correct!</h5>
-            <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
-          </div>
-        </div>) 
-        : 
-        (<div className="incorrect-ans-div">
-          <h1>Oops... Try Again!</h1>
+    <>
+    {q === 0 && !rewardClaimed ? (<>
+      <div className="correct-ans-div">
+        <h1>Good Job!</h1>
+        <div>
           <QuestionCard currentTrivia={currentTrivia}/>
-          <h5>Correct Answer:</h5>
+          <h5>Correct!</h5>
           <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
-        </div>)}
-      </>) 
-      : (<>
-    <h1>{currentTrivia.category?.toUpperCase()}</h1>
-    <QuestionCard currentTrivia={currentTrivia}/>
-    {currentTrivia.a ? (<AnswerCard ans={currentTrivia.a} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>) : 
-      (<AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>)}
-
-    {currentTrivia.b ? (<AnswerCard ans={currentTrivia.b} currentTrivia={currentTrivia} resp={'b'} handleClick={handleClick} id={id}/>) : 
-      (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'b'} handleClick={handleClick} id={id}/></>)}
-
-    {currentTrivia.c ? (<AnswerCard ans={currentTrivia.c} currentTrivia={currentTrivia} resp={'c'} handleClick={handleClick} id={id}/>) : 
-      (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'c'} handleClick={handleClick} id={id}/></>)}
-
-    {currentTrivia.d ? (<AnswerCard ans={currentTrivia.d} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/>) : 
-      (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/></>)}
+        </div>
+        <button className="claim-reward-btn">Claim Reward!</button>
+      </div>
+    </>) : (<>
+      <div className="game-page-main">
+        {displayWin ? 
+        (<>
+          {correct ? 
+          (<div className="correct-ans-div">
+            <h1>Good Job!</h1>
+            <div>
+              <QuestionCard currentTrivia={currentTrivia}/>
+              <h5>Correct!</h5>
+              <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
+            </div>
+          </div>) 
+          : 
+          (<div className="incorrect-ans-div">
+            <h1>Oops... Try Again!</h1>
+            <QuestionCard currentTrivia={currentTrivia}/>
+            <h5>Correct Answer:</h5>
+            <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
+          </div>)}
+        </>) 
+        : (<>
+      <h1>{currentTrivia.category?.toUpperCase()}</h1>
+      <p>question {q}/10</p>
+      <QuestionCard currentTrivia={currentTrivia}/>
+      {currentTrivia.a ? (<AnswerCard ans={currentTrivia.a} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>) : 
+        (<AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>)}
+  
+      {currentTrivia.b ? (<AnswerCard ans={currentTrivia.b} currentTrivia={currentTrivia} resp={'b'} handleClick={handleClick} id={id}/>) : 
+        (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'b'} handleClick={handleClick} id={id}/></>)}
+  
+      {currentTrivia.c ? (<AnswerCard ans={currentTrivia.c} currentTrivia={currentTrivia} resp={'c'} handleClick={handleClick} id={id}/>) : 
+        (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'c'} handleClick={handleClick} id={id}/></>)}
+  
+      {currentTrivia.d ? (<AnswerCard ans={currentTrivia.d} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/>) : 
+        (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/></>)}
+        
+        </>)}
+      </div>
       
       </>)}
-    </div>
+    </>
   );
 }
 
