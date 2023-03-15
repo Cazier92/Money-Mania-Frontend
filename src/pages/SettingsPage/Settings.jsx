2f5b6f0258ad1a18f34410b9175fb93de5d93e61
cbowman422 <chrisb0wman@hotmail.com>
2023-03-15 15:19:37 -0400

    buttons and main text for settings page

--- src/pages/SettingsPage/Settings.jsx
+++ src/pages/SettingsPage/Settings.jsx
@@ -1,7 +1,46 @@
+import './Settings.css'
 const Settings = () => {
+
+
+
   return (
   <>
-  <h1>this is settings page </h1>
+  <h3>Sound</h3>
+  <div class="switch-toggle">
+    <div class="button-check" id="button-check">
+      <input type="checkbox" class="checkbox"></input>
+      <span class="switch-btn"></span>
+      <span class="layer"></span>
+    </div>
+  </div>
+  <h3>Music</h3>
+  <div class="switch-toggle">
+    <div class="button-check" id="button-check1">
+      <input type="checkbox" class="checkbox"></input>
+      <span class="switch-btn"></span>
+      <span class="layer"></span>
+    </div>
+  </div>
+  <h3>Vibrations</h3>
+  <div class="switch-toggle">
+    <div class="button-check" id="button-check2">
+      <input type="checkbox" class="checkbox"></input>
+      <span class="switch-btn"></span>
+      <span class="layer"></span>
+    </div>
+  </div>
+  <h3>Notifications</h3>
+  <div class="switch-toggle">
+    <div class="button-check" id="button-check3">
+      <input type="checkbox" class="checkbox"></input>
+      <span class="switch-btn"></span>
+      <span class="layer"></span>
+    </div>
+  </div>
+  <div>
+    <button className="signOut"> Sign Out </button> 
+  </div>
+
   </> );
 }
 
