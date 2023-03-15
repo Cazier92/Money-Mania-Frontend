5017742183b9dc52f0474db9329bfa7b1430e01c
Cazier92 <bryce.cazier@gmail.com>
2023-03-15 13:38:33 -0700

    update rendering after answering a question

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -9,7 +9,18 @@ const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpda
   const [displayWin, setDisplayWin] = useState(false)
   const [correct, setCorrect] = useState(false)
   const [id, setId] = useState('')
-  const fillerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  const fillerText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  let answer
+
+  if (currentTrivia.ans === 'a') {
+    answer = currentTrivia.a
+  } else if (currentTrivia.ans === 'b') {
+    answer = currentTrivia.b
+  } else if (currentTrivia.ans === 'c') {
+    answer = currentTrivia.c
+  } else if (currentTrivia.ans === 'd') {
+    answer = currentTrivia.d
+  }
 
   const handleClick = (resp) => {
     if (currentTrivia.ans === resp) {
@@ -25,20 +36,31 @@ const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpda
     }
   }
 
+  
+
   return ( 
     <div className="game-page-main">
       {displayWin ? 
       (<>
         {correct ? 
-        (<div>
-          <h1>Correct!</h1>
+        (<div className="correct-ans-div">
+          <h1>Good Job!</h1>
+          <div>
+            <QuestionCard currentTrivia={currentTrivia}/>
+            <h5>Correct!</h5>
+            <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
+          </div>
         </div>) 
         : 
-        (<div>
-          <h1>Wrong Answer! Try Again Next Time!</h1>
+        (<div className="incorrect-ans-div">
+          <h1>Oops... Try Again!</h1>
+          <QuestionCard currentTrivia={currentTrivia}/>
+          <h5>Correct Answer:</h5>
+          <AnswerCard ans={answer} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>
         </div>)}
       </>) 
-      : (<></>)}
+      : (<>
+    <h1>{currentTrivia.category?.toUpperCase()}</h1>
     <QuestionCard currentTrivia={currentTrivia}/>
     {currentTrivia.a ? (<AnswerCard ans={currentTrivia.a} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>) : 
       (<AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'a'} handleClick={handleClick} id={id}/>)}
@@ -51,6 +73,8 @@ const GamePage = ({currentTrivia, updateData, handleUpdateProfile, user, setUpda
 
     {currentTrivia.d ? (<AnswerCard ans={currentTrivia.d} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/>) : 
       (<><AnswerCard ans={fillerText} currentTrivia={currentTrivia} resp={'d'} handleClick={handleClick} id={id}/></>)}
+      
+      </>)}
     </div>
   );
 }
