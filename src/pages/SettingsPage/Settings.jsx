7b0b40a31ff66b1f621f9a73093258547c648140
cbowman422 <chrisb0wman@hotmail.com>
2023-03-15 16:17:24 -0400

    settings and settings css

--- src/pages/SettingsPage/Settings.jsx
+++ src/pages/SettingsPage/Settings.jsx
@@ -1,47 +1,65 @@
 import './Settings.css'
+import { Link } from "react-router-dom";
 const Settings = () => {
 
-
+  const clearLocalStorage = ({signOut}) =>{
+    localStorage.clear();
+    signOut();
+  }
 
   return (
-  <>
-  <h3>Sound</h3>
-  <div class="switch-toggle">
-    <div class="button-check" id="button-check">
-      <input type="checkbox" class="checkbox"></input>
-      <span class="switch-btn"></span>
-      <span class="layer"></span>
+  <div className="settingsContainer">
+    <div className="settingsTitle">
+    <h3 className="settingsTextTitle" >Settings</h3>
+    </div>
+  <div className="flexSettings">
+    <h3 className="settingsText">Sound</h3>
+    <div class="switch-toggle">
+      <div class="button-check" id="button-check">
+        <input type="checkbox" class="checkbox"></input>
+        <span class="switch-btn"></span>
+        <span class="layer"></span>
+      </div>
     </div>
   </div>
-  <h3>Music</h3>
-  <div class="switch-toggle">
-    <div class="button-check" id="button-check1">
-      <input type="checkbox" class="checkbox"></input>
-      <span class="switch-btn"></span>
-      <span class="layer"></span>
+  <div className="flexSettings">
+    <h3 className="settingsText">Music</h3>
+    <div class="switch-toggle">
+      <div class="button-check" id="button-check1">
+        <input type="checkbox" class="checkbox"></input>
+        <span class="switch-btn"></span>
+        <span class="layer"></span>
+      </div>
     </div>
   </div>
-  <h3>Vibrations</h3>
-  <div class="switch-toggle">
-    <div class="button-check" id="button-check2">
-      <input type="checkbox" class="checkbox"></input>
-      <span class="switch-btn"></span>
-      <span class="layer"></span>
+  <div className="flexSettings"> 
+    <h3 className="settingsText">Vibrations</h3>
+    <div class="switch-toggle">
+      <div class="button-check" id="button-check2">
+        <input type="checkbox" class="checkbox"></input>
+        <span class="switch-btn"></span>
+        <span class="layer"></span>
+      </div>
     </div>
   </div>
-  <h3>Notifications</h3>
-  <div class="switch-toggle">
-    <div class="button-check" id="button-check3">
-      <input type="checkbox" class="checkbox"></input>
-      <span class="switch-btn"></span>
-      <span class="layer"></span>
+  <div className="flexSettings">
+    <h3 className="settingsText" >Notifications</h3>
+    <div class="switch-toggle">
+      <div class="button-check" id="button-check3">
+        <input type="checkbox" class="checkbox"></input>
+        <span class="switch-btn"></span>
+        <span class="layer"></span>
+      </div>
     </div>
-  </div>
-  <div>
-    <button className="signOut"> Sign Out </button> 
   </div>
 
-  </> );
+  <div className="signOutContainer">
+    <Link to={'/'}>
+    <button className="signOut" onClick={clearLocalStorage}> Sign Out </button> 
+    </Link>
+  </div>
+
+  </div> );
 }
 
 export default Settings;
