db2b839b38ec4e7aad494cb41d0b974b97c5c2a2
jordbort <jordan.bortner@gmail.com>
2023-03-16 13:12:26 -0400

    Updating button styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Banner from "../../components/Banner/Banner";
 import PiggyBank from "../../components/PiggyBank/PiggyBank";
 import continueTrivia from "../../buttons/continue-trivia.svg"
+import changeCat from "../../buttons/change-category.svg"
 
 const Home = ({ currentCategory, handleChangeTrivia, userProfile }) => {
     return (
@@ -14,7 +15,7 @@ const Home = ({ currentCategory, handleChangeTrivia, userProfile }) => {
                     <div className="home-btn-div">
                         <h2>Category: {currentCategory}</h2>
                         <Link to='/gamepage'><img src={continueTrivia} onClick={handleChangeTrivia} alt="continue" /></Link>
-                        <Link to='/categories'><button className='homeButton'>Change Category</button></Link>
+                        <Link to='/categories'><img src={changeCat} alt="change" /></Link>
                     </div>
                 </>) : (<div className="home-btn-div">
                     <Link to='/categories'><button className='homeButton'>Choose Category</button></Link>
